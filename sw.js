a31aa818715d33b975379f95481c07151d6e7253
chokorad <148511350+chokorad@users.noreply.github.com>
2026-07-29 20:53:10 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "lector-gamer-v7";
+const CACHE = "lector-gamer-v8";
 const SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
